fe0e18716a7b700912a723353695f92e1e7817d4
Jucielly Silva <juciellysouzasilva@gmail.com>
2020-05-18 12:43:01 -0300

    rendering pokemons on screen done

--- app.js
+++ app.js
@@ -12,16 +12,17 @@ const fetchPokemon = () => {
             //console.log(pokemons)
 
             const listPokemons = pokemons.reduce((accumulator, pokemon) => {
-                const types = pokemon.types.map((typeInfo => typeInfo.type.name))
+                const types = pokemon.types.map(typeInfo => typeInfo.type.name)
 
-                accumulator += `<li class="card>
-                <img class="card-image ${types[0]}" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
+                accumulator += `<li class="card ${types[0]}">
+                <img class="card-image" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
                 <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
                 <p class="card-subtitle">${types.join(" | ")}</p>
                 </li>`
+
+                return accumulator
             }, "")
             const ul = document.querySelector('[data-js="pokedex"]')
-
             ul.innerHTML = listPokemons
         })
 
