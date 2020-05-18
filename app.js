93fd573c48b49ca938cbfa9bc87a74de71a8bbfd
Jucielly Silva <juciellysouzasilva@gmail.com>
2020-05-18 11:39:55 -0300

    pokeApi fetch done

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const fetchPokemon = () => {
-    const url = `https://pokeapi.co/api/v2/pokemon/`
-    fetch(url)
+    const getPokemonUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+    const pokemonPromises = []
+
+    for (let i = 1; i <= 100; i++) {
+        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
+    }
+
+    Promise.all(pokemonPromises)
+    .then(pokemons => {
+        console.log(pokemons)
+    })
+
 }
 
 fetchPokemon()
